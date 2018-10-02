442e518770148b0ced9a9f8d72647211d04ac542
Marie <>
2018-10-02 16:38:52 +0000

    obsah trojuhelniku

--- pythagoras.php
+++ pythagoras.php
@@ -3,4 +3,11 @@
 $a = "2";
 $b = "5";
 $obdelnikobsah = $a * $b;
-echo "Obsah obdélníku o stranách $a cm a $b cm je $obdelnikobsah cm2.";
+echo "Obsah obdélníku o stranách $a cm a $b cm je $obdelnikobsah cm2.";?>
+<!doctype html>
+<p><?php
+$strana = "6";
+$uhel = "60";
+$vyska = sin(deg2rad($uhel))*$strana;
+$trojuhelnikobsah = round(($vyska * $strana) / 2);
+echo "Rovnostranný trojúhelník o straně délky $strana cm a úhlu $uhel stupňů je $trojuhelnikobsah cm2.";?></p>
